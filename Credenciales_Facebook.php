f451ba97ced6c4ab9a01508fe9532e70c113fb83
Antonio <cpapremiumgroup@gmail.com>
2021-10-08 13:05:54 -0700

    Update Credenciales_Facebook.php

--- Credenciales_Facebook.php
+++ Credenciales_Facebook.php
@@ -1,6 +1,6 @@
 <?php
-	$login_url = "https://viralrich.cash/FacebookAPI/Verificador_Tokens.php";
-	$app_id = "229590942480326";
-	$app_secret = "82d5fd2641d5bf049c74081e12e5d8e8";
+	$login_url = "https://URI_AUTH_REDIRECT/Verificador_Tokens.php";
+	$app_id = "APPID";
+	$app_secret = "SECRET";
 	$permissions = ['public_profile,groups_access_member_info,publish_to_groups,read_insights']; // permissions
 ?>
